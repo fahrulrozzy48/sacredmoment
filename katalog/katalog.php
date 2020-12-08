24d906932bf90e5ac5bd08aeaea32768a6ac3015
fahrulrozzy48 <38930865+fahrulrozzy48@users.noreply.github.com>
2020-12-08 17:19:03 +0700

    upload katalog

---
+++ katalog/katalog.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Sacred Moment | Desain Katalog</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+
+
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js"></script>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js"></script>
+
+
+    <!--Plugins Css-->
+    <link rel="stylesheet" href="css/plugins.css">
+    <!--Main Style Css-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Modernizr Js-->
+    <script src="js/modernizr.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122650090-5"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-122650090-5');
+    </script>
+
+
+
+    <link rel="stylesheet" href="css/stylekatalog.css">
+</head>
+
+<body>
+
+    <div class="header">
+        <nav>
+            <input id="nav-toggle" type="checkbox">
+            <div class="logo">
+                <img src="../img/logo.png" alt="">
+            </div>
+            <ul class="links">
+                <li><a href="../">Beranda</a></li>
+                <li><a href="katalog.php">Desain Katalog</a></li>
+                <li><a href="../pricelist/pricelist.php">Daftar Harga</a></li>
+                <li><a href="../fq/fq.php">F & Q</a></li>
+                <li><a href="../kontak/kontak.php">Kontak</a></li>
+            </ul>
+            <label for="nav-toggle" class="icon-burger">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </label>
+        </nav>
+
+
+        <div id="main">
+
+            <div class="ct-main-content ct-index">
+
+
+                <div id="scroll-content">
+                </div>
+
+                <!--PAGE CONTENT START-->
+                <div class="page-content">
+
+                    <!--PIXI SLIDER START-->
+                    <div id="pixi-slider">
+
+                        <div class="slider-wrapper">
+                            <!--SLIDER ITEM-->
+                            <div class="slider-item">
+                                <div class="slide-image">
+                                    <img src="images/projects/project1/bgprojek1.jpg" class="slide-item__image"
+                                        alt="Project Image">
+                                </div>
+
+                                <div class="slide-content">
+                                    <div class="slide-content-inner">
+                                        <div class="item-cat">The Sacred Moment</div>
+                                        <h2 class="item-title" data-splitting>Design Simple.</h2>
+                                        <a href="contoh_katalog/projek_simple/coversimple.php"
+                                            class="item-link link-underline cursor-link" target="s_blank">View
+                                            Project</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--SLIDER ITEM-->
+                            <div class="slider-item">
+                                <div class="slide-image">
+                                    <img src="images/projects/project2/bgprojek2.jpg" class="slide-item__image"
+                                        alt="Project Image">
+                                </div>
+                                <div class="slide-content">
+                                    <div class="slide-content-inner">
+                                        <div class="item-cat">The Sacred Moment</div>
+                                        <h2 class="item-title" data-splitting>Design Basic.</h2>
+                                        <a href="contoh_katalog/projek_basic/coverbasic.php"
+                                            class="item-link link-underline cursor-link" target="_blank">View
+                                            Project</a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <!--SLIDER ITEM-->
+                            <div class="slider-item">
+                                <div class="slide-image">
+                                    <img src="images/projects/project3/bgprojek3.jpg" class="slide-item__image"
+                                        alt="Project Image">
+                                </div>
+                                <div class="slide-content">
+                                    <div class="slide-content-inner">
+                                        <div class="item-cat">The Sacred Moment</div>
+                                        <h2 class="item-title" data-splitting>Design Standard.</h2>
+                                        <a href="contoh_katalog/projek_standard/coverstandard.php" target="_blank"
+                                            class="item-link link-underline cursor-link">View Project</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--SLIDER ITEM-->
+                            <div class="slider-item">
+                                <div class="slide-image">
+                                    <img src="images/projects/project4/bgprojek4.jpg" class="slide-item__image"
+                                        alt="Project Image">
+                                </div>
+
+                                <div class="slide-content">
+                                    <div class="slide-content-inner">
+                                        <div class="item-cat">The Sacred Moment</div>
+                                        <h2 class="item-title" data-splitting>Design Premium.</h2>
+                                        <a href="contoh_katalog/projek_premium/coverpremium.php" target="_blank"
+                                            class="item-link link-underline cursor-link">View Project</a>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+
+                        <!--PIXI SLIDER NAVIGATION START-->
+                        <div class="arrows-wrap">
+                            <div class="slider-nav slider-nav--prev parallax-wrap" data-nav="previous">
+                                <div class="parallax-icon">
+                                    <i class="fas fa-chevron-left"></i>
+                                </div>
+                            </div>
+                            <div class="slider-nav slider-nav--next parallax-wrap" data-nav="next">
+                                <div class="parallax-icon">
+                                    <i class="fas fa-chevron-right"></i>
+                                </div>
+                            </div>
+                            <div class="progress-bar">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <!--PIXI SLIDER NAVIGATION END-->
+
+                        <div id="canvas-container"></div>
+
+                    </div>
+                    <!--PIXI SLIDER END-->
+
+                </div>
+                <!--PAGE CONTENT END-->
+
+
+
+
+                <!--Jquery JS-->
+                <script src="js/jquery.min.js"></script>
+                <!--Plugins JS-->
+                <script src="js/plugins.min.js"></script>
+                <!--Google Map Api-->
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUU5FZiF5WLFFfgIC1n64Zr0zfpQZjBBg">
+                </script>
+                <!--Site Main JS-->
+                <script src="js/main.js"></script>
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
